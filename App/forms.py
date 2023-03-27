99f1c872da057e474f6ef1ebe3dde995e015bf1a
jade <moyosoreoluwamafo@gmail.com>
2023-03-27 15:21:31 +0100

    designed signup form

--- App/forms.py
+++ App/forms.py
@@ -1,2 +1,20 @@
 from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+
+class CreateUserForm(UserCreationForm):
+
+    class Meta:
+        model = User
+
+        fields = [
+            'first_name', 'last_name', 'email', 'username', 'password', 'password2'
+        ]
+
+        labels = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email Address',
+        }
--- App/views.py
+++ App/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 
 # Create your views here.
 
 def homepage(request):
     return render(request, 'layout.html')
 
-
+# Authenticate Login Request
 def loginUser(request):
 
     if request.method == 'POST':
@@ -25,16 +27,27 @@ def loginUser(request):
                     
     else:
         return render(request, 'login.html')
-
-
+    
+# Authenticate Logout Request
 def logoutUser(request):
 
     logout(request)
     messages.success(request, ("You have been logged out!"))
     return redirect('login')
 
+
 def signup(request):
-    return render(request, 'signup.html')
+
+    form = CreateUserForm
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+
+    return render(request, 'signup.html', {'form':form})
+    
 
 
 def deposit(request):

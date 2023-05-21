71c6353c6cfd10330f29fe2262e7a24d2d7a60e3
thejade <moyosoreoluwamafo@gmail.com>
2023-05-21 17:53:12 +0100

    cryp

--- BankApp/settings.py
+++ BankApp/settings.py
@@ -82,13 +82,20 @@ WSGI_APPLICATION = 'BankApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'BankApp',
+#         'USER': 'postgres',
+#         'PASSWORD': 'thejade',
+#         'HOST': 'localhost',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'BankApp',
-        'USER': 'postgres',
-        'PASSWORD': 'thejade',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3.db',
     }
 }
 

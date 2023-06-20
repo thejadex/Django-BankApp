73b81caf5391e332825ce459e406cad7f51cccd4
thejade <moyosoreoluwamafo@gmail.com>
2023-06-20 21:02:09 +0100

    commit

--- App/models.py
+++ App/models.py
@@ -32,11 +32,11 @@ class EncryptedField(models.TextField):
 # This Model creates a random account number for each user registered and their balance.
 class UserAccount(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=30)
-    # username = EncryptedField(max_length=255)
+    # username = models.CharField(max_length=30)
+    username = EncryptedField(max_length=255)
     account_balance = models.FloatField(default=0)
-    account_number = models.CharField(max_length=10, unique=True)
-    # account_number = EncryptedField(max_length=255, unique=True)
+    # account_number = models.CharField(max_length=10, unique=True)
+    account_number = EncryptedField(max_length=255, unique=True)
 
     def __str__(self):
         return self.user.username

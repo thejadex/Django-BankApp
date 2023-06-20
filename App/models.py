8ecef9291a1df1c9c6133e3d6b46d0477b9d0ccd
thejade <moyosoreoluwamafo@gmail.com>
2023-06-20 19:30:54 +0100

    removed decryption files

--- App/models.py
+++ App/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from .AES_encryption_whole import aes_combined
-from .AES_DECRYPT import decrypt
+# from .AES_DECRYPT import decrypt
 
 
 """
@@ -20,10 +20,10 @@ class EncryptedField(models.TextField):
             return aes_combined(value)  # Encrypt the value
         return value
 
-    def to_python(self, value):
-        if value is not None:
-            return decrypt(value)  # Decrypt the value
-        return value
+    # def to_python(self, value):
+    #     if value is not None:
+    #         return decrypt(value)  # Decrypt the value
+    #     return value
 
     def get_prep_value(self, value):
         if value is not None:
@@ -32,11 +32,11 @@ class EncryptedField(models.TextField):
 # This Model creates a random account number for each user registered and their balance.
 class UserAccount(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # username = models.CharField(max_length=30)
-    username = EncryptedField(max_length=255)
+    username = models.CharField(max_length=30)
+    # username = EncryptedField(max_length=255)
     account_balance = models.FloatField(default=0)
-    # account_number = models.CharField(max_length=10, unique=True)
-    account_number = EncryptedField(max_length=255, unique=True)
+    account_number = models.CharField(max_length=10, unique=True)
+    # account_number = EncryptedField(max_length=255, unique=True)
 
     def __str__(self):
         return self.user.username
@@ -56,11 +56,11 @@ class UserAccount(models.Model):
         self.account_balance -= amount
         self.save()
 
-    @property
-    def decrypted_account_number(self):
-        # Decrypt the account number and return the plaintext value
-        decrypted = decrypt(self.account_number)
-        return decrypted
+    # @property
+    # def decrypted_account_number(self):
+    #     # Decrypt the account number and return the plaintext value
+    #     decrypted = decrypt(self.account_number)
+    #     return decrypted
 
     # @property
     # def decrypted_username(self):

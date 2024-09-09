5c3b40ed7448bad3f66a32a7c494353ab615687d
adminJade <thejade@lotusbank.com>
2024-09-09 11:03:03 +0100

    fixed encryption bug

--- App/models.py
+++ App/models.py
@@ -3,38 +3,32 @@ import random
 from django.db import models
 from django.contrib.auth.models import User
 
-<<<<<<< HEAD
-=======
-from .AES_encryption_whole import aes_combined
+# from .AES_encryption_whole import aes_combined
 # from .AES_DECRYPT import decrypt
 
->>>>>>> b9ec106b50624b71c510edf81f5a0f8f5079c58c
 
 """
     Note that these models are fields that are created in the database.
 """
 
-<<<<<<< HEAD
-=======
-class EncryptedField(models.TextField):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+# class EncryptedField(models.TextField):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
 
-    def from_db_value(self, value, expression, connection):
-        if value is not None:
-            return aes_combined(value)  # Encrypt the value
-        return value
+#     def from_db_value(self, value, expression, connection):
+#         if value is not None:
+#             return aes_combined(value)  # Encrypt the value
+#         return value
 
-    # def to_python(self, value):
-    #     if value is not None:
-    #         return decrypt(value)  # Decrypt the value
-    #     return value
+#     # def to_python(self, value):
+#     #     if value is not None:
+#     #         return decrypt(value)  # Decrypt the value
+#     #     return value
 
-    def get_prep_value(self, value):
-        if value is not None:
-            return aes_combined(value)  # Encrypt the value
+#     def get_prep_value(self, value):
+#         if value is not None:
+#             return aes_combined(value)  # Encrypt the value
 
->>>>>>> b9ec106b50624b71c510edf81f5a0f8f5079c58c
 # This Model creates a random account number for each user registered and their balance.
 class UserAccount(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -60,8 +54,6 @@ class UserAccount(models.Model):
         self.account_balance -= amount
         self.save()
 
-<<<<<<< HEAD
-=======
     # @property
     # def decrypted_account_number(self):
     #     # Decrypt the account number and return the plaintext value
@@ -73,7 +65,6 @@ class UserAccount(models.Model):
     #     # Decrypt the balance and return the plaintext value
     #     decrypted = decrypt(self.username)
     #     return decrypted
->>>>>>> b9ec106b50624b71c510edf81f5a0f8f5079c58c
 
 
 # This model is used to allow users deposit into their accounts and their account balance is updated.
